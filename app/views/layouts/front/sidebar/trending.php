02469019cf501c70b04b6ded46a6a7681cf0d654
pcofilada <pcofilada@gmail.com>
2014-06-07 03:26:49 +0800

    Remove 1 trending item

--- app/views/layouts/front/sidebar/trending.php
+++ app/views/layouts/front/sidebar/trending.php
@@ -82,18 +82,3 @@
 		</div>
 	</div>
 </div>
-
-<div class="trending clearfix">
-	<div class="image col-md-4">
-		<img src="/assets/images/trending.jpg" alt="" style="width: 100%; height: 70px; ">
-	</div>
-	<div class="info col-md-8">
-		<h4 class="title">Title here</h4>
-		<h5 class="location">Pasig City, Manila</h5>
-		<div class="status">
-			<span><i class="fa fa-check-circle-o"></i> 34</span>
-			<span><i class="fa fa-times-circle-o"></i> 1</span>
-			<span><i class="fa fa-comments-o"></i> 2</span>
-		</div>
-	</div>
-</div>
