761023c352ca33e36f17b897b258522a4eaf66a5
pcofilada <pcofilada@gmail.com>
2014-06-07 21:11:52 +0800

    Display trending image

--- app/views/layouts/front/sidebar/trending.php
+++ app/views/layouts/front/sidebar/trending.php
@@ -1,7 +1,7 @@
 <?php foreach ($trending as $trending) { ?>
 	<div class="trending clearfix">
 		<div class="image col-md-4">
-			<img src="<?php echo $trending[0]['image'][0]; ?>" alt="" style="width: 100%; height: 70px; ">
+			<img src="<?php echo $trending['image'][0]; ?>" alt="" style="width: 100%; height: 70px; ">
 		</div>
 		<div class="info col-md-8">
 			<h4 class="title"><?php echo $trending->title; ?></h4>
