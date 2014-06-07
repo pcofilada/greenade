05bc4b557f94f2dd5feafc3a9541f4176597bd02
Rommel David <rommel@gorated.com>
2014-06-08 02:00:09 +0800

    Added search box for jessie

--- app/views/front/index.php
+++ app/views/front/index.php
@@ -1,4 +1,6 @@
-<div class="col-sm-offset-3 col-md-6 "><input id="pac-input" class="controls" type="text" placeholder="Search Box"></div>
-<div class="col-md-1"><button class="controls btn btn-success">GO!</button></div>
-<textarea id="map_data" class="hidden"><?php echo str_replace('\/','/',json_encode($reports)); ?></textarea>
-<div id="map" style="width: 100%; height: 100%;"></div>
+<form method="post" id="geocoding_form">
+	<div class="col-sm-offset-3 col-md-6 input"><input id="pac-input address" class="controls" type="text" placeholder="Search Box" name="address"></div>
+	<div class="col-md-1"><input type="submit" class="controls btn btn-success" value="Search" ></div>
+</form>
+	<textarea id="map_data" class="hidden"><?php echo str_replace('\/','/',json_encode($reports)); ?></textarea>
+	<div id="map" style="width: 100%; height: 100%;"></div>
