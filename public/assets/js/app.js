5655458833d325018a638d2982d6df894cba7844
Jessie Arevalo <jessie@gorated.com>
2014-06-07 12:12:08 +0800

    gmap add marker

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,17 +1,71 @@
 $(document).ready(function(){
+
 	var map = new GMaps({
 	  div: '#map',
 	  lat: -12.043333,
-	  lng: -77.028333
+	  lng: -77.028333,
+	  draggable: true,
+	  click: function(e){
+	  	var latLng = e.latLng;
+		console.log(JSON.stringify(latLng));
+		var fLat = latLng.k;
+		var fLng = latLng.A;
+	  	initialize(latLng);
+	  },
+	  infoWindow: {
+		  content: '<p>form</p>'
+		},
+	  map:map
 	});
 
+	/*
 	map.addMarker({
-           	  lat: -12.043333,
-	  lng: -77.028333
-           });
+        lat: -12.043333,
+	  	lng: -77.028333,
+	  	click:function(){
+	  		console.log('test');
+	  	},
+	  	infoWindow: {
+		  content: '<p>HTML Content</p>'
+		}
+    });
+	*/
 
-	map.addMarker({
-           	  lat: -12.043333,
-	  lng: -77.034
-           });
+//gmap events
+
+	var location;
+	
+	function initialize(oData) {
+		if(typeof(oData) !='undefined'){
+			map.addMarker({
+				lat: oData.k,
+				lng: oData.A,
+				draggable: true,
+				infoWindow: {
+					content: "<p>TAPOS NA!</p>"
+				}
+			});
+		}
+		/*
+	        google.maps.event.addListener(map, 'click', function(event) {
+	            mapZoom = map.getZoom();
+	            startLocation = event.latLng;
+	            setTimeout("placeMarker()", 600);
+	        });
+	    map.addMarker({
+	        lat: -12.043333,
+		  	lng: -77.028333,
+		  	click:function(){
+		  		console.log('test');
+		  	},
+		  	infoWindow: {
+			  content: '<p>HTML Content</p>'
+			}
+	    });
+		*/
+    }
+
+
+	initialize();
+
 });
