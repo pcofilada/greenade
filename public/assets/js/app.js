8f25ac65a90a0e1c147de1d1ad67a1487d340638
Jessie Arevalo <jessie@gorated.com>
2014-06-08 02:29:36 +0800

    fix geolocation of modal

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -106,23 +106,23 @@ $(document).ready(function(){
 			  }
 			});
 
+			GMaps.geolocate({
+			  success: function(position) {
+			  	console.log(JSON.stringify(position));
+			    map_modal.setCenter(position.coords.latitude, position.coords.longitude);
+			  },
+			  error: function(error) {
+			    console.log('Geolocation failed: '+error.message);
+			  },
+			  not_supported: function() {
+			    console.log("Your browser does not support geolocation");
+			  },
+			  always: function() {
+			    console.log("Done!");
+			  }
+			});
 		},500)
 
-	GMaps.geolocate({
-	  success: function(position) {
-	  	console.log(JSON.stringify(position));
-	    map_modal.setCenter(position.coords.latitude, position.coords.longitude);
-	  },
-	  error: function(error) {
-	    console.log('Geolocation failed: '+error.message);
-	  },
-	  not_supported: function() {
-	    console.log("Your browser does not support geolocation");
-	  },
-	  always: function() {
-	    console.log("Done!");
-	  }
-	});
 
 
 	}
