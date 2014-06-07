51ed51add7fa5e85e73147f1cdd3cdbc37de006e
pcofilada <pcofilada@gmail.com>
2014-06-07 11:33:28 +0800

    Style user list icons

--- app/views/admin/user/index.php
+++ app/views/admin/user/index.php
@@ -1,26 +1,28 @@
-<h1>Users</h1>
-<table class="table table-striped">
-	<thead>
-		<th>Name</th>
-		<th>Mobile Number</th>
-		<th>Email</th>
-		<th></th>
-	</thead>
-	<tbody>
-		<?php foreach ($users as $user) { ?>
-			<tr>
-				<td><?php echo $user->name; ?></td>
-				<td><?php echo $user->mobile; ?></td>
-				<td><?php echo $user->email; ?></td>
-				<td>
-					<div class="icons right">
-					<a href="#"><i class="fa fa-pencil-square-o"></i> Edit</a> |
-			                       <form class="delete" action="" method="POST">
-						<i class="fa fa-trash-o"></i><input type="submit" value="Delete" />
-			                       </form>
-					</div>
-				</td>
-			</tr>
-		<?php } ?>
-	</tbody>
-</table>
+<div class="users">
+	<h1>Users</h1>
+	<table class="table table-striped">
+		<thead>
+			<th>Name</th>
+			<th>Mobile Number</th>
+			<th>Email</th>
+			<th></th>
+		</thead>
+		<tbody>
+			<?php foreach ($users as $user) { ?>
+				<tr>
+					<td><?php echo $user->name; ?></td>
+					<td><?php echo $user->mobile; ?></td>
+					<td><?php echo $user->email; ?></td>
+					<td>
+						<div class="icons right">
+						<a href="#"><i class="fa fa-pencil-square-o"></i> Edit</a> |
+				                       <form class="delete" action="" method="POST">
+							<i class="fa fa-trash-o"></i><input type="submit" value="Delete" />
+				                       </form>
+						</div>
+					</td>
+				</tr>
+			<?php } ?>
+		</tbody>
+	</table>
+</div>
