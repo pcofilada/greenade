66fc7e4513c88ac28c84d0f675227b931dc58aca
pcofilada <pcofilada@gmail.com>
2014-06-07 18:01:39 +0800

    Remove facebook login

--- app/views/layouts/front/index.php
+++ app/views/layouts/front/index.php
@@ -113,6 +113,60 @@
             </div>
           </div>
         </div>
+
+            <div id="map_report" class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+      <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+             <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+            <h2 class="modal-title" id="contribute-modalLabel">Report</h2>
+          </div>
+          <div class="modal-body">
+              <form class="form-horizontal" role="form" action="/user/<?php echo $user->name ?>/report" method="POST" enctype="multipart/form-data">
+                    <div class="form-group">
+                      <div class="col-sm-10 pull-right">
+                        <div id="map_modal" style="width: 100%; height: 300px;"></div>
+                      </div>
+                    </div>
+                   <div class="form-group">
+                     <label for="name" class="col-sm-2 control-label">Title</label>
+                     <div class="col-sm-10">
+                       <input type="text" class="form-control" name="title" id="title" placeholder="Title">
+                     </div>
+                   </div>
+                  <div class="form-group">
+                    <label for="mobile" class="col-sm-2 control-label">Description</label>
+                    <div class="col-sm-10">
+                      <textarea type="text" class="form-control" name="description" id="description" placeholder="Description" ></textarea>
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <label for="mobile" class="col-sm-2 control-label"> Image</label>
+                    <div class="col-sm-10">
+                      <input name="image[]" type="file" multiple/>
+                    </div>
+                  </div>
+                  <div class="form-group">
+                     <div class="col-sm-10">
+                       <input type="text" class="form-control hidden" name="long" id="long" >
+                       <input type="text" class="form-control hidden" name="lat" id="lat" >
+                     </div>
+                  </div>
+                  </div>
+                  <div class="form-group">
+                    <div class="col-sm-offset-2 col-sm-10">
+                      <input type="submit" class="btn btn-primary" value="REPORT">
+                      <button type="button" class="btn btn-default" data-dismiss="modal">CANCEL</button>
+                    </div>
+                  </div>
+                  <br> 
+                  <br>
+                </form>
+          </div>
+        </div>
+      </div>
+    </div>    
+    
     <?php }else{ ?>
         <div class="modal fade" id="login-modal" tabindex="-1" role="dialog" aria-labelledby="login-modalLabel" aria-hidden="true">
           <div class="modal-dialog">
@@ -216,59 +270,6 @@
       </div>
     </div>
 
-    <div id="map_report" class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
-      <div class="modal-dialog modal-lg">
-        <div class="modal-content">
-             <div class="modal-header">
-            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-            <h2 class="modal-title" id="contribute-modalLabel">Report</h2>
-          </div>
-          <div class="modal-body">
-              <form class="form-horizontal" role="form" action="/user/<?php echo $user->name ?>/report" method="POST" enctype="multipart/form-data">
-                    <div class="form-group">
-                      <div class="col-sm-10 pull-right">
-                        <div id="map_modal" style="width: 100%; height: 300px;"></div>
-                      </div>
-                    </div>
-                   <div class="form-group">
-                     <label for="name" class="col-sm-2 control-label">Title</label>
-                     <div class="col-sm-10">
-                       <input type="text" class="form-control" name="title" id="title" placeholder="Title">
-                     </div>
-                   </div>
-                  <div class="form-group">
-                    <label for="mobile" class="col-sm-2 control-label">Description</label>
-                    <div class="col-sm-10">
-                      <textarea type="text" class="form-control" name="description" id="description" placeholder="Description" ></textarea>
-                    </div>
-                  </div>
-                  <div class="form-group">
-                    <label for="mobile" class="col-sm-2 control-label"> Image</label>
-                    <div class="col-sm-10">
-                      <input name="image[]" type="file" multiple/>
-                    </div>
-                  </div>
-                  <div class="form-group">
-                     <div class="col-sm-10">
-                       <input type="text" class="form-control hidden" name="long" id="long" >
-                       <input type="text" class="form-control hidden" name="lat" id="lat" >
-                     </div>
-                  </div>
-                  </div>
-                  <div class="form-group">
-                    <div class="col-sm-offset-2 col-sm-10">
-                      <input type="submit" class="btn btn-primary" value="REPORT">
-                      <button type="button" class="btn btn-default" data-dismiss="modal">CANCEL</button>
-                    </div>
-                  </div>
-                  <br> 
-                  <br>
-                </form>
-          </div>
-        </div>
-      </div>
-    </div>    
-
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
@@ -277,86 +278,5 @@
     <script src="/assets/js/gmaps.js"></script>
     <script src="/assets/js/app.js"></script>
     <script src="/assets/js/Chart.js"></script>
-    <script>
-  // This is called with the results from from FB.getLoginStatus().
-  function statusChangeCallback(response) {
-    console.log('statusChangeCallback');
-    console.log(response);
-    // The response object is returned with a status field that lets the
-    // app know the current login status of the person.
-    // Full docs on the response object can be found in the documentation
-    // for FB.getLoginStatus().
-    if (response.status === 'connected') {
-      // Logged into your app and Facebook.
-      testAPI();
-    } else if (response.status === 'not_authorized') {
-      // The person is logged into Facebook, but not your app.
-      document.getElementById('status').innerHTML = 'Please log ' +
-        'into this app.';
-    } else {
-      // The person is not logged into Facebook, so we're not sure if
-      // they are logged into this app or not.
-      document.getElementById('status').innerHTML = 'Please log ' +
-        'into Facebook.';
-    }
-  }
-
-  // This function is called when someone finishes with the Login
-  // Button.  See the onlogin handler attached to it in the sample
-  // code below.
-  function checkLoginState() {
-    FB.getLoginStatus(function(response) {
-      statusChangeCallback(response);
-    });
-  }
-
-  window.fbAsyncInit = function() {
-  FB.init({
-    appId      : '510357489064889',
-    cookie     : true,  // enable cookies to allow the server to access 
-                        // the session
-    xfbml      : true,  // parse social plugins on this page
-    version    : 'v2.0' // use version 2.0
-  });
-
-  // Now that we've initialized the JavaScript SDK, we call 
-  // FB.getLoginStatus().  This function gets the state of the
-  // person visiting this page and can return one of three states to
-  // the callback you provide.  They can be:
-  //
-  // 1. Logged into your app ('connected')
-  // 2. Logged into Facebook, but not your app ('not_authorized')
-  // 3. Not logged into Facebook and can't tell if they are logged into
-  //    your app or not.
-  //
-  // These three cases are handled in the callback function.
-
-  FB.getLoginStatus(function(response) {
-    statusChangeCallback(response);
-  });
-
-  };
-
-  // Load the SDK asynchronously
-  (function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));
-
-  // Here we run a very simple test of the Graph API after login is
-  // successful.  See statusChangeCallback() for when this call is made.
-  function testAPI() {
-    console.log('Welcome!  Fetching your information.... ');
-    FB.api('/me', function(response) {
-      console.log('Successful login for: ' + response.name);
-      document.getElementById('status').innerHTML =
-        'Thanks for logging in, ' + response.name + '!';
-    });
-  }
-</script>
-
   </body>
 </html>
