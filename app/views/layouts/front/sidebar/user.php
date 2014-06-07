4bf5ce1da5c07dcc6e9dc8d4fd59882c513ef276
pcofilada <pcofilada@gmail.com>
2014-06-08 00:06:12 +0800

    Remove charts on user

--- app/views/layouts/front/sidebar/user.php
+++ app/views/layouts/front/sidebar/user.php
@@ -1,7 +1,6 @@
 <ul class="nav nav-pills nav-stacked">
 	<li><a href="" class="active"><i class="fa fa-map-marker"></i> Map</a></li>
 	<li><a href=""><i class="fa fa-file-text-o"></i> Reports</a></li>
-	<li><a href=""><i class="fa fa-bar-chart-o"></i> Charts</a></li>
 	<li><a href=""><i class="fa fa-comment-o"></i> Notification</a></li>
 	<li><a href=""><i class="fa fa-envelope-o"></i>  Messages</a></li>
 	<li><a href=""><i class="fa fa-bolt"></i> Quest</a></li>
