980422f0a2042a4c221fe57fea8926032107b076
Rommel David <rommel@gorated.com>
2014-06-07 18:39:15 +0800

    Added search button

--- app/views/front/index.php
+++ app/views/front/index.php
@@ -1,1 +1,3 @@
+<div class="col-sm-offset-3 col-md-6 "><input id="pac-input" class="controls" type="text" placeholder="Search Box"></div>
+<div class="col-md-1"><button class="controls btn btn-success">GO!</button></div>
 <div id="map" style="width: 100%; height: 100%;"></div>
