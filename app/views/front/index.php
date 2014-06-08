c9cc4b155fd8bd4b6f5d515c853455e229aef8e4
Jessie Arevalo <jessie@gorated.com>
2014-06-08 11:29:48 +0800

    remove conflict signs

--- app/views/front/index.php
+++ app/views/front/index.php
@@ -3,29 +3,6 @@
 	<div class="col-md-1"><input type="submit" class="controls-button btn btn-success" value="GO!" ></div>
 </form>
 
-<<<<<<< Updated upstream
-	<textarea id="map_data" class="hidden"><?php echo str_replace('\/','/',json_encode($reports)); ?></textarea>
-	<div id="map" style="width: 100%; height: 100%;"></div>
-
-
-  <select id="date-filter" class="selectpicker">
-	<option value="0"> ALL </option>
-	<option value="1">JAN</option>
-	<option value="2">FEB </option>
-	<option value="3">MAR</option>
-	<option value="4">APR</option>
-	<option value="5">MAY</option>
-	<option value="6">JUN</option>
-	<option value="7">JUL</option>
-	<option value="8">AUG</option>
-	<option value="9">SEP</option>
-	<option value="10">OCT</option>
-	<option value="11">NOV</option>
-	<option value="12">DEC</option>
-  </select>
-  <select id="year-filter" class="selectpicker">
-  </select>
-=======
 <div class="col-sm-offset-2 col-md-6">
 	<div class="col-md-3">
 	  <select id="date-filter" class="selectpicker form-control controls-select">
@@ -59,4 +36,3 @@
 
 	});
 </script>
->>>>>>> Stashed changes
