341f976fcc1eb62d10d652441646e2c47ea47cdc
pcofilada <pcofilada@gmail.com>
2014-06-08 14:19:09 +0800

    Style filters

--- app/views/mobile/index.php
+++ app/views/mobile/index.php
@@ -1,32 +1,31 @@
-<form method="post" id="geocoding_form" class="col-md-6">
+<div class="filters">
+	<form method="post" id="geocoding_form" class="col-sm-6">
+	<div class="col-sm-7 input"><input id="address" class="controls" type="text" placeholder="Search" name="address"></div>
+	<div class="col-sm-3"><input id="search_btn" type="submit" class="controls-button btn btn-success" value="GO!" ></div>
+	</form> 
 
-<div class="col-md-6 input"><input id="address" class="controls" type="text" placeholder="Search" name="address"></div>
-<div class="col-md-3"><input id="search_btn" type="submit" class="controls-button btn btn-success" value="GO!" ></div>
-</form> 
-
-<div class="col-md-6">
-	<div class="col-md-3">
-	  <select id="date-filter" class="selectpicker form-control controls-select">
-		<option value="0"> -- ALL -- </option>
-		<option value="1">JAN</option>
-		<option value="2">FEB </option>
-		<option value="3">MAR</option>
-		<option value="4">APR</option>
-		<option value="5">MAY</option>
-		<option value="6">JUN</option>
-		<option value="7">JUL</option>
-		<option value="8">AUG</option>
-		<option value="9">SEP</option>
-		<option value="10">OCT</option>
-		<option value="11">NOV</option>
-		<option value="12">DEC</option>
-	  </select>
-	</div>
-	<div class="col-md-1">
-	</div>
-	<div class="col-md-3">
-	  <select id="year-filter" class="selectpicker form-control controls-select">
-	  </select>
+	<div class="col-sm-6">
+		<div class="col-sm-6">
+		  <select id="date-filter" class="selectpicker form-control controls-select">
+			<option value="0"> -- ALL -- </option>
+			<option value="1">JAN</option>
+			<option value="2">FEB </option>
+			<option value="3">MAR</option>
+			<option value="4">APR</option>
+			<option value="5">MAY</option>
+			<option value="6">JUN</option>
+			<option value="7">JUL</option>
+			<option value="8">AUG</option>
+			<option value="9">SEP</option>
+			<option value="10">OCT</option>
+			<option value="11">NOV</option>
+			<option value="12">DEC</option>
+		  </select>
+		</div>
+		<div class="col-sm-6">
+		  <select id="year-filter" class="selectpicker form-control controls-select">
+		  </select>
+		</div>
 	</div>
 </div>
 <textarea id="map_data" class="hidden"><?php echo str_replace('\/','/',json_encode($reports)); ?></textarea>
